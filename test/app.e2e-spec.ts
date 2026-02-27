59a6487fc4edc13a7a4aaee1d847e5c110adbedf
Aiman Muzafar <aimanmuzaffar95@gmail.com>
2026-02-27 13:16:58 +0500

    Add e2e test

---
+++ test/app.e2e-spec.ts
@@ -0,0 +1,31 @@
+import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import request from 'supertest';
+import { App } from 'supertest/types';
+import { AppController } from '../src/app.controller';
+import { AppService } from '../src/app.service';
+
+describe('AppController Simple (e2e)', () => {
+  let app: INestApplication<App>;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [AppService],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('/ (GET)', async () => {
+    await request(app.getHttpServer())
+      .get('/')
+      .expect(200)
+      .expect('Hello Aiman!');
+  });
+});
